ff7cb9301f665839eb46ec0306e9f77a32b8f82f
aoleary <seanm187@gmail.com>
2022-04-24 08:53:57 +0000

    [SQUASH] Binder: prepare for rebase by reverting existing
    Revert "binder: fix race that allows malicious free of live buffer"
    
    This reverts commit e45fbc42de2e7394859ccf3f34660b1edd277a2e.
    
    Revert "coredump: fix race condition between mmget_not_zero()/get_task_mm() and core dumping"
    
    This reverts commit abf4847c69a73847693dbf6bbb66b90c0b504b3c.
    
    Revert "binder: make FIFO inheritance a per-context option"
    
    This reverts commit f9d834a31c7f5cff3114a741613c09040450fd01.
    
    Revert "Update IPC Binder"
    
    This reverts commit c0e27cc09b1bbeacd233d971e9cda679147cd323.
    
    Revert "Add Android IPC Binder"
    
    This reverts commit 0266629299959994efc270091e257c3b8fc68230.

--- fs/userfaultfd.c
+++ fs/userfaultfd.c
@@ -441,8 +441,6 @@ static int userfaultfd_release(struct inode *inode, struct file *file)
 	 * taking the mmap_sem for writing.
 	 */
 	down_write(&mm->mmap_sem);
-	if (!mmget_still_valid(mm))
-		goto skip_mm;
 	prev = NULL;
 	for (vma = mm->mmap; vma; vma = vma->vm_next) {
 		cond_resched();
@@ -465,7 +463,6 @@ static int userfaultfd_release(struct inode *inode, struct file *file)
 		vma->vm_flags = new_flags;
 		vma->vm_userfaultfd_ctx = NULL_VM_UFFD_CTX;
 	}
-skip_mm:
 	up_write(&mm->mmap_sem);
 	mmput(mm);
 wakeup:
@@ -767,8 +764,6 @@ static int userfaultfd_register(struct userfaultfd_ctx *ctx,
 		goto out;
 
 	down_write(&mm->mmap_sem);
-	if (!mmget_still_valid(mm))
-		goto out_unlock;
 	vma = find_vma_prev(mm, start, &prev);
 	if (!vma)
 		goto out_unlock;
@@ -914,8 +909,6 @@ static int userfaultfd_unregister(struct userfaultfd_ctx *ctx,
 		goto out;
 
 	down_write(&mm->mmap_sem);
-	if (!mmget_still_valid(mm))
-		goto out_unlock;
 	vma = find_vma_prev(mm, start, &prev);
 	if (!vma)
 		goto out_unlock;
