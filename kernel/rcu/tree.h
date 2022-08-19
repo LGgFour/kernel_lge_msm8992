e9abd8d944b044960d2e396047ce2ed8ed2f0a83
steadfasterX <steadfasterX@gmail.com>
2022-08-19 16:25:03 +0200

    Revert "rcu: Make RCU_FAST_NO_HZ take advantage of numbered callbacks"
    This reverts commit be441f00076ecb7b337ec766698e956586c43e33.
    
    fix camera FC's on pie
    
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>
    Change-Id: Ie95d9285baa88d784ed5e58ddcf2dd6a66a55894
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>
    
    Conflicts:
    	kernel/rcu/tree.h
    	kernel/rcu/tree_plugin.h

--- kernel/rcu/tree.h
+++ kernel/rcu/tree.h
@@ -89,13 +89,18 @@ struct rcu_dynticks {
 	int dynticks_nmi_nesting;   /* Track NMI nesting level. */
 	atomic_t dynticks;	    /* Even value for idle, else odd. */
 #ifdef CONFIG_RCU_FAST_NO_HZ
-	bool all_lazy;		    /* Are all CPU's CBs lazy? */
+	int dyntick_drain;	    /* Prepare-for-idle state variable. */
+	unsigned long dyntick_holdoff;
+				    /* No retries for the jiffy of failure. */
+	struct timer_list idle_gp_timer;
+				    /* Wake up CPU sleeping with callbacks. */
+	unsigned long idle_gp_timer_expires;
+				    /* When to wake up CPU (for repost). */
+	bool idle_first_pass;	    /* First pass of attempt to go idle? */
 	unsigned long nonlazy_posted;
 				    /* # times non-lazy CBs posted to CPU. */
 	unsigned long nonlazy_posted_snap;
 				    /* idle-period nonlazy_posted snapshot. */
-	unsigned long last_accelerate;
-				    /* Last jiffy CBs were accelerated. */
 	unsigned long last_advance_all;
 				    /* Last jiffy CBs were all advanced. */
 	int tick_nohz_enabled_snap; /* Previously seen value from sysfs. */
